f48a3df78481eaccd79439c4e3188abe0bee52bf
SBhattacharya45 <umyadeepb27@gmail.com>
2020-06-24 18:21:50 +0530

    changes for uploading to heroku

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const Blog = require("./models/blog");
 mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set('view engine' , 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '25mb'}));
+app.use(bodyParser.urlencoded({limit: '25mb', extended: true}));
 
 app.get('/', (req,res) => {
     res.render('index');
